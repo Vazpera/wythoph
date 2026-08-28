77a7513dc03cce92fbae55ba38d88d8d8b514b57
vazpera <jthoesel10@hotmail.com>
2026-08-28 07:48:59 -0700

    Cleaning up devenv.nix

--- devenv.nix
+++ devenv.nix
@@ -46,20 +46,8 @@
     scripts       # List scripts
   '';
 
-  # https://devenv.sh/tasks/
-  # tasks = {
-  #   "myproj:setup".exec = "mytool build";
-  #   "devenv:enterShell".after = [ "myproj:setup" ];
-  # };
-
-  # https://devenv.sh/tests/
   enterTest = ''
     echo "Running tests"
     git --version | grep --color=auto "${pkgs.git.version}" echo
   '';
-
-  # https://devenv.sh/git-hooks/
-  # git-hooks.hooks.shellcheck.enable = true;
-
-  # See full reference at https://devenv.sh/reference/options/
 }
